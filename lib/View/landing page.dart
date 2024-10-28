37870c31f98cdc4ccb93a447dcb82f90f08292e7
ShraddhaDeshmukh10 <shraddha10122002@gmail.com>
2024-10-28 13:06:56 +0530

    Updated

--- lib/View/landing page.dart
+++ lib/View/landing page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/Actions/connector.dart';
+import 'package:flutter_application_1/View/connector.dart';
 
 class LandingPage extends StatelessWidget {
   final List fetchProducts;
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:async_redux/async_redux.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/Actions/connector.dart';
+import 'package:flutter_application_1/View/connector.dart';
 import 'package:flutter_application_1/appstate.dart';
 
 void main() {
